3ee471db4b51082bac295c4d66d8f1bc5fc73c7a
Robert Schrenk <robert@schrenk.cc>
2025-04-02 13:30:18 +0200

    Upgrade API for Moodle 4.5

--- classes/text_filter.php
+++ classes/text_filter.php
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-namespace classes;
+namespace filter_h5p;
 
 defined('MOODLE_INTERNAL') || die;
 
